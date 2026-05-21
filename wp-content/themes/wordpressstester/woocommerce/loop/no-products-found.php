632f411c09c700c314b4425a91c3b6ab11b0979a
cooper8k <orlanprado.c@gmail.com>
2026-05-21 18:39:42 -0400

    correción a tarjetas para que se vean alineadas, y precios alineados hacia abajo

--- wp-content/themes/wordpressstester/woocommerce/loop/no-products-found.php
+++ wp-content/themes/wordpressstester/woocommerce/loop/no-products-found.php
@@ -39,7 +39,7 @@ $shop_url = get_permalink( wc_get_page_id( 'shop' ) );
 		<div class="nxt-no-products__actions">
 			<a href="<?php echo esc_url( $shop_url ); ?>" class="nxt-no-products__btn nxt-no-products__btn--primary">
 				<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true"><polyline points="15 18 9 12 15 6"/></svg>
-				<?php esc_html_e( 'Ver todos los productos', 'woocommerce' ); ?>
+				<?php esc_html_e( 'Explora otros productos', 'woocommerce' ); ?>
 			</a>
 			<?php
 			$contact_page = get_page_by_path( 'contacto' );
